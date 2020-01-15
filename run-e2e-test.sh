e548ad63cf92cb75ae49643b1de0b3151744abe9
Nitishkumar Singh <nitishkumarsingh71@gmail.com>
2020-01-15 08:44:19 +0530

    Signed-off-by: Nitishkumar Singh <nitishkumarsingh71@gmail.com>
    adding support to run e2e tests on Kubernetes with k3d
    
    Capability to run e2e test has been added on docker swarm and kubernetes.
    Kubernetes environment is setup using k3sup to install k3s.
    
    We can use run-e2e-test.sh to execute all the tests locally.
    
    We can look into issue https://github.com/openfaas/certifier/issues/25 for complete informtion.

---
+++ run-e2e-test.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+if ! [ -x "$(command -v docker)" ]; then
+  echo 'Unable to find docker command, please install Docker (https://www.docker.com/) and retry' >&2
+  exit 1
+fi
+
+export IP=127.0.0.1
+export USER_NAME=$(whoami)
+# setup docker swarm 
+docker swarm init
+git clone https://github.com/openfaas/faas.git
+./faas/deploy_stack.sh --no-auth
+
+# run test in docker swarm
+export OPENFAAS_URL=http://$IP:8080/
+make test-swarm
+
+# remove docker swarm, once tests complete
+docker swarm leave --force
+
+#setup k3s using k3sup
+curl -sLS https://get.k3sup.dev | sh
+install k3sup /usr/local/bin/
+k3sup install --ip $IP --user $USER_NAME
+k3sup app install openfaas
+
+# run test in k3s
+export OPENFAAS_URL=http://$IP:31112/
+make test-kubernetes
