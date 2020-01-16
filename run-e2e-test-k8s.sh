e3f63169983ad605390f0a35cb6d9dd4e490d43a
Nitishkumar Singh <nitishkumarsingh71@gmail.com>
2020-01-16 08:49:33 +0530

    Signed-off-by: Nitishkumar Singh <nitishkumarsingh71@gmail.com>
    split docker swarm and k8 test script

---
+++ run-e2e-test-k8s.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if ! [ -x "$(command -v docker)" ]; then
+  echo 'Unable to find docker command, please install Docker (https://www.docker.com/) and retry' >&2
+  exit 1
+fi
+
+export IP=127.0.0.1
+export USER_NAME=$(whoami)
+
+# setup k3s using k3sup
+sudo curl -sLS https://get.k3sup.dev | sh
+sudo install k3sup /usr/local/bin/
+k3sup install --ip $IP --user $USER_NAME --local
+k3sup app install openfaas
+
+# run test in k3s
+export OPENFAAS_URL=http://$IP:31112/
+make test-kubernetes
