46b0416cbbd662a571fca7abc87b79f72cb661ec
Your Name <nitishkumar.singh@quantiphi.com>
2020-02-06 10:25:09 +0530

    export kubeconfig after moving config

--- run-e2e-test-k8s.sh
+++ run-e2e-test-k8s.sh
@@ -7,15 +7,16 @@ fi
 
 export IP=127.0.0.1
 export USER_NAME=$(whoami)
-export KUBECONFIG_PATH=/home/$USER_NAME/.kube
-export KUBECONFIG=$KUBECONFIG_PATH/config
+export KUBECONFIG_FOLDER_PATH=/home/$USER_NAME/.kube/
+export KUBECONFIG_PATH=$KUBECONFIG_FOLDER_PATH/config
 
 # setup k3s using k3sup
 sudo curl -sLS https://get.k3sup.dev | sh
 sudo install k3sup /usr/local/bin/
-mkdir -p $KUBECONFIG_PATH
+mkdir -p $KUBECONFIG_FOLDER_PATH
 k3sup install --local --ip $IP --user $USER_NAME
-cp `pwd`/kubeconfig $KUBECONFIG
+cp `pwd`/kubeconfig $KUBECONFIG_PATH
+export KUBECONFIG=$KUBECONFIG_PATH
 
 # disable basic auth
 k3sup app install openfaas --basic-auth=false
